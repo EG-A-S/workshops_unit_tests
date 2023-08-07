39b6f1cdd1efd917bd4f85a7d8916a7f7677ae17
Maciej Kowalczyk <makow@eg.dk>
2023-08-07 14:33:33 +0200

    Test scan gitleaks 3

--- src/main/java/com/example/javamavenjunithelloworld/CalculatorApp.java
+++ src/main/java/com/example/javamavenjunithelloworld/CalculatorApp.java
@@ -14,7 +14,7 @@ public class CalculatorApp {
     public static void main(String[] args) {
         Calculator calc = new Calculator();
         String api = "Secret: AIDAJ7YAA3VVO32BHLDO";
-        String password = "Secret: Asdfaw2341!D!S@!";
+        String password = "Secret: aGVsbG8taS1hbS1hLXRlc3Qta2V5";
         int a = 1;
         int b = 2;
         int c = calc.add(a, b);
